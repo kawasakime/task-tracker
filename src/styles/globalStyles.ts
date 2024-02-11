d1316e47973c520a9ff3ceee258e0fc2c7bb8107
wyylame <wyylame@yandex.ru>
2024-02-11 19:08:01 +0300

    created global styles

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  scrollViewJustifyBetween: {
+    flexGrow: 1,
+    paddingBottom: 15,
+    justifyContent: 'space-between',
+  },
+});
