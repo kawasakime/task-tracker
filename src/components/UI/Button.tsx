111295ed765fc3c270ee4f32b2c5640167dae43a
wyylame <wyylame@yandex.ru>
2024-02-11 19:14:08 +0300

    changed button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -4,10 +4,19 @@ import {colors} from '../../constants/variables';
 import {StyledButton, StyledText} from '.';
 import {ButtonProps} from '../../interfaces/main.interfaces';
 
-const Button = ({children, onPress, ...rest}: ButtonProps) => {
+const Button = ({
+  children,
+  onPress,
+  disabled,
+  textStyle,
+  ...rest
+}: ButtonProps) => {
   return (
-    <StyledButton onPress={onPress} {...rest}>
-      <StyledText style={styles.text}>{children}</StyledText>
+    <StyledButton
+      style={disabled && styles.disabled}
+      onPress={onPress}
+      {...rest}>
+      <StyledText style={[styles.text, textStyle]}>{children}</StyledText>
     </StyledButton>
   );
 };
@@ -15,6 +24,10 @@ const Button = ({children, onPress, ...rest}: ButtonProps) => {
 const styles = StyleSheet.create({
   text: {
     color: colors.white,
+    fontWeight: 'bold',
+  },
+  disabled: {
+    opacity: 0.7,
   },
 });
 
