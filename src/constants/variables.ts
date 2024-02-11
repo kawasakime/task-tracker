d2bee4cf2fc885592483397204259467fc1851a3
wyylame <wyylame@yandex.ru>
2024-02-11 15:19:22 +0300

    added colors variables

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -2,7 +2,9 @@ export const colors = {
   white: '#ffffff',
   black: '#101419',
   primary: '#476C9B',
+  success: '#06BA63',
   danger: '#984447',
+  cardBackground: '#F5F5F5',
 };
 
 export const radius = {
